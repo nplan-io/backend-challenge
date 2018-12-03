7685e6797fc0a2496eb635b8b24d914cc3736321
Nick Smith <nicksmithr@icloud.com>
2018-12-03 12:07:43 +0000

    Simplify Django models

--- app/app/models.py
+++ app/app/models.py
@@ -6,15 +6,8 @@ class Project(models.Model):
     """ Project model """
 
     name = models.CharField(max_length=100)
-    location = models.CharField(max_length=100, default="")
     created = models.DateTimeField(auto_now=True)
     uid = models.UUIDField(default=uuid.uuid4, unique=True)
-    product_phase = models.CharField(
-        max_length=10,
-        choices=WEEKDAYS,
-        default=1)
-    owned_by = models.ForeignKey(User, null=True,
-                                 on_delete=models.DO_NOTHING, default=None)
 
     def __str__(self):
         return self.name
@@ -27,11 +20,7 @@ class Schedule(models.Model):
     project = models.ForeignKey(Project, on_delete=models.CASCADE, null=True,
                                 related_name="schedules")
     status = models.CharField(max_length=50, default="New")
-    language = models.CharField(max_length=5, null=True)
     schedule_file = models.FileField(upload_to='schedule_files/', default=None)
-    progress_name = models.CharField(max_length=100, default="Baseline")
-    owned_by = models.ForeignKey(settings.AUTH_USER_MODEL, null=True,
-                                 on_delete=models.DO_NOTHING, default=None)
     deadline = models.DateTimeField(default=None, null=True)
 
 
